c52134158a04b03ad9822a08c7be2f3c729fef67
John Laurence Salinas <johnlaurence.salinas@gmail.com>
2026-03-04 14:16:22 +0800

    quick fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dashboard_page.dart';
 import 'progress_page.dart';
 import 'result_page.dart';
 import 'practice_page.dart';
-import 'package:ispeak/learning_resources_page.dart';
+import 'package:ispeak/learning_resources_page.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'iSpeak',
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const MainPage(),
     );
   }
@@ -30,60 +32,61 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int _currentIndex = 0;
-  int _progressPageVersion = 0;
 
-  Widget _buildPage() {
-    switch (_currentIndex) {
-      case 0:
-        return DashBoardPage(
-          onStartPractice: () => setState(() => _currentIndex = 1),
-          onLearningResources: () => setState(() => _currentIndex = 4),
-        );
-      case 1:
-        return PracticePage(
-          onFinish: () => setState(() => _currentIndex = 3),
-        );
-      case 2:
-        return ProgressPage(key: ValueKey(_progressPageVersion));
-      case 3:
-        return ResultPage(
-          onBackToHome: () => setState(() => _currentIndex = 0),
-        );
-      case 4:
-        return LearningResourcesScreen(
-          onBack: () => setState(() => _currentIndex = 0),
-        );
-      default:
-        return DashBoardPage(
-          onStartPractice: () => setState(() => _currentIndex = 1),
-          onLearningResources: () => setState(() => _currentIndex = 4),
-        );
-    }
+  late final List<Widget> _pages;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pages = [
+      DashBoardPage(
+        onStartPractice: () => setState(() => _currentIndex = 1),
+        onLearningResources: () => setState(() => _currentIndex = 4),
+      ),
+      PracticePage(
+        onFinish: () => setState(() => _currentIndex = 3),
+      ),
+      const ProgressPage(),
+      ResultPage(
+        onBackToHome: () => setState(() => _currentIndex = 0),
+      ),
+      LearningResourcesScreen(
+        onBack: () => setState(() => _currentIndex = 0),
+      ),
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
+    bool hideBars = _currentIndex == 3;
+
     return Scaffold(
       backgroundColor: const Color(0xFFF5F5F5),
-      body: _buildPage(),
+
+      // 🔥 THIS IS THE IMPORTANT CHANGE
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: _currentIndex == 3
+      floatingActionButton: hideBars
           ? null
           : SizedBox(
               height: 65,
               width: 65,
               child: FittedBox(
                 child: FloatingActionButton(
-                  onPressed: () {
-                    setState(() => _currentIndex = 1);
-                  },
+                  onPressed: () => setState(() => _currentIndex = 1),
                   backgroundColor: const Color(0xFF3F7CF4),
                   elevation: 6,
                   child: const Icon(Icons.mic, size: 28, color: Colors.white),
                 ),
               ),
             ),
-      bottomNavigationBar: _currentIndex == 3
+
+      bottomNavigationBar: hideBars
           ? null
           : BottomAppBar(
               shape: const CircularNotchedRectangle(),
@@ -96,62 +99,42 @@ class _MainPageState extends State<MainPage> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    // Home
-                    InkWell(
-                      onTap: () => setState(() => _currentIndex = 0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.home,
-                            size: 26,
-                            color: _currentIndex == 0 ? const Color(0xFF3F7CF4) : Colors.grey,
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            'Home',
-                            style: TextStyle(
-                              fontSize: 11,
-                              color: _currentIndex == 0 ? const Color(0xFF3F7CF4) : Colors.grey,
-                              fontWeight: _currentIndex == 0 ? FontWeight.w600 : FontWeight.normal,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                    _buildNavItem(Icons.home, 'Home', 0),
                     const SizedBox(width: 80),
-                    // Progress
-                    InkWell(
-                      onTap: () => setState(() {
-                        _currentIndex = 2;
-                        _progressPageVersion++;
-                      }),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.show_chart,
-                            size: 26,
-                            color: _currentIndex == 2 ? const Color(0xFF3F7CF4) : Colors.grey,
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            'Progress',
-                            style: TextStyle(
-                              fontSize: 11,
-                              color: _currentIndex == 2 ? const Color(0xFF3F7CF4) : Colors.grey,
-                              fontWeight: _currentIndex == 2 ? FontWeight.w600 : FontWeight.normal,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                    _buildNavItem(Icons.show_chart, 'Progress', 2),
                   ],
                 ),
               ),
             ),
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, String label, int index) {
+    bool isSelected = _currentIndex == index;
+
+    return InkWell(
+      onTap: () => setState(() => _currentIndex = index),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            size: 26,
+            color: isSelected ? const Color(0xFF3F7CF4) : Colors.grey,
+          ),
+          const SizedBox(height: 4),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 11,
+              color: isSelected ? const Color(0xFF3F7CF4) : Colors.grey,
+              fontWeight:
+                  isSelected ? FontWeight.w600 : FontWeight.normal,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
